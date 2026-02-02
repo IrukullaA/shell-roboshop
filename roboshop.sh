37c4cc83f2f8405f20b8edae79894d3652a7cd3f
Abhinay Irukulla <irukulla.dba@gmail.com>
2026-02-01 17:12:39 -0800

    File was not saved, so i have to save it 1st

---
+++ roboshop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SG_ID="sg-0515c35ee5d56743a"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in "$@";
+do
+    instance_id=$(
+        aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --count 1 \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]' \
+        --query "Instances[0].InstanceId" \
+        --output text
+    )
+
+    if [ $instance_id == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance--ids $instance_id \
+            --query "Reservations[].Instances[0].PublicIpAddress" \
+            --output text
+        )
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance--ids $instance_id \
+            --query "Reservations[].Instances[0].PrivateIpAddress" \
+            --output text
+        )
+    fi    
+done
